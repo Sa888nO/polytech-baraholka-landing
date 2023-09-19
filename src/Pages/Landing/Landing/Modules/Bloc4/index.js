e0784d83ef1c4d9e139e17b0d65bbda487db2d09
Alex E <egorushkin.a.s@mail.ru>
2023-09-19 18:03:03 +0300

    delete not used content

--- src/Pages/Landing/Landing/Modules/Bloc4/index.js
+++
@@ -1,1 +0,0 @@
-export * from "./Bloc4";
--- src/Pages/Landing/Landing/Modules/Footer/Footer.jsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-import styles from "./Footer.module.scss";
-export const Footer = () => {
-
-    return (
-        <div>
-        </div>
-    );
-};
